3b0ec2e41177e77909e1978ffd3265f3e415de0e
Cristian Castaneda <core.editor>
2021-02-13 16:40:43 +0800

    RWD
    Mobile Design
    481 x 767

--- js/app.js
+++ js/app.js
@@ -1,8 +1,5 @@
 // for the mouse cursor
 let mouseCursor = document.querySelector('.cursor');
-let links = document.querySelectorAll('.personal .for-cursor span a');
-console.log(links);
-
 
 window.addEventListener('mousemove', cursor);
 
@@ -10,12 +7,3 @@ function cursor(e) {
     mouseCursor.style.top = e.pageY + 'px';
     mouseCursor.style.left = e.pageX + 'px';
 }
-
-links.forEach(link => {
-    link.addEventListener('mouseleave', () => {
-        mouseCursor.classlist.remove("link-grow");
-    });
-    link.addEventListener('mouseover', () => {
-        mouseCursor.classlist.add("link-grow");
-    });
-});
